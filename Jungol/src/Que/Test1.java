8179caed906a5e0f169c5731386f225636650700
gotjs0730 <77846811+gotjs0730@users.noreply.github.com>
2021-10-05 15:18:21 +0900

    test1

---
+++ Jungol/src/Que/Test1.java
@@ -0,0 +1,33 @@
+package Que;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		double inp = sc.nextDouble();
+		sc.close();
+		
+		if(inp<=50.08) {
+			System.out.println("Flyweight");
+		}else if(inp <= 61.23) {
+			System.out.println("Lightweight");
+		}
+		else if(inp <= 72.57) {
+			System.out.println("Middleweight");
+		}
+		else if(inp <= 88.45) {
+			System.out.println("Cruiserweight");
+		}
+		else{
+			System.out.println("Heavyweight");
+		}
+		
+		
+		
+
+	}
+
+}
